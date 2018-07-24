3b26e4abe3ac84338e5479319f72a911aae17566
Edward P. Legaspi <czetsuya@gmail.com>
2018-07-24 17:58:34 +0800

    Set sequence.

--- src/main/java/com/broodcamp/javaee6/model/Member.java
+++ src/main/java/com/broodcamp/javaee6/model/Member.java
@@ -21,6 +21,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -40,7 +41,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 public class Member implements Serializable {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
